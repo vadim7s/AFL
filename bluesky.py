877902b52c0e3bca7ba7db963bffd5a2e0dfc7cd
Vadim Shumilov <shumilov7@gmail.com>
2019-10-24 08:29:20 +1100

    minor changes to test if remote has been updated

--- bluesky.py
+++ bluesky.py
@@ -17,11 +17,13 @@ class Bluesky:
         self.line_count_list = [] #this gets a list of how many lines each file has
         for table in self.tables:
             self.line_count_list.append(sum(1 for line in open(self.folder+'/'+table,encoding="utf8")))        
-        self.dfs = []  # list of Dataframes representing first X rows of each table
+        self.dfs = []  # list of Dataframes representing first X rows of each table where X is defined by the class parameter max_rows
         for table in self.tables:
             self.dfs.append(pd.read_csv(self.folder+'/'+table,sep=self.separator ,parse_dates=True,nrows=self.max_rows))
             
 # create an instance
-my_class = Bluesky('./bluesky/Adventureworks','.txt','\t')
+#my_class = Bluesky('./bluesky/Adventureworks','.txt','\t')
+my_class = Bluesky('./Adventureworks','.txt','\t')
+
 print(my_class.dfs[1])
 
