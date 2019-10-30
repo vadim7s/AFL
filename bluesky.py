363f14ec1c0d3b2785aa91d9eb7e89d8655bfba2
Vadim Shumilov <shumilov7@gmail.com>
2019-10-30 13:47:01 +1100

    initial looping

--- bluesky.py
+++ bluesky.py
@@ -22,7 +22,7 @@ def scan_columns(dfs,tables,entity):
     '''
     this function loops through a DF's columns and puts it all in one dataframe
     '''
-    result=pd.DataFrame(columns=['Table_Name','Column_Name','Column_Type','Distinct_Values','Null_Count','Entity_Flag'])
+    result=pd.DataFrame(columns=['Table_Name','Column_Name','Column_Type','Length','Distinct_Values','Null_Count','Entity_Flag'])
     for df,table in zip(dfs,tables):
         for column in df.columns:
             #first try to convert to dates
@@ -35,6 +35,7 @@ def scan_columns(dfs,tables,entity):
             null_count=df[column].isna().sum()
             df[column]=df[column].dropna() 
             values=df[column].to_list()
+            length=len(values)
             distinct_values = list(set(values))
             #types = list(set([type(x) for x in distinct_values]))
             types = str(df[column].dtype)
@@ -42,7 +43,7 @@ def scan_columns(dfs,tables,entity):
                 entity_flag=1
             else:
                 entity_flag=0
-            new_line=pd.DataFrame({'Table_Name':[table],'Column_Name':[column],'Column_Type':[types],'Distinct_Values':[len(distinct_values)],'Null_Count':[null_count],'Entity_Flag':[entity_flag]})
+            new_line=pd.DataFrame({'Table_Name':[table],'Column_Name':[column],'Column_Type':[types],'Length':[length],'Distinct_Values':[len(distinct_values)],'Null_Count':[null_count],'Entity_Flag':[entity_flag]})
             result = pd.concat([result,new_line])
             pass
     return result
@@ -63,12 +64,24 @@ class Bluesky:
         self.column_scan=scan_columns(self.dfs,self.tables,self.entity)
             
 # create an instance
-my_class = Bluesky('./bluesky/Adventureworks','.txt','\t','Customer.txt')
-#my_class = Bluesky('./Adventureworks','.txt','\t')
+#my_class = Bluesky('./bluesky/Adventureworks','.txt','\t','Customer.txt')
+my_class = Bluesky('./Adventureworks','.txt','\t','Customer.txt')
 
 # not set tables to loop for matching
 df=my_class.column_scan
 entity_columns = df[df['Entity_Flag']==1]
+entity_columns.to_csv('Entity.csv',index=False)
 loop_columns = df[df['Entity_Flag']==0]
-pass
+loop_columns.to_csv('Loop.csv',index=False)
+
+for i, row in entity_columns.iterrows():
+    # conditions to take a column as a possible key
+    if row[5]==0:   # there should not be nulls 
+        if row[2][0:3]!='date' and row[2][0:4]!='float':  # it cannot be a date or float
+            if row[3]==row[4]: # all available values should be unique
+                # now for columns which are good candidates for IDs try to match to other tables
+                for j, tabl in loop_columns.iterrows():
+                    if tabl[5]==0 and tabl[2][0:3]!='date' and tabl[2][0:4]!='float' and  tabl[3]==tabl[4]:
+                        print('for row ',row[1],' found possible candidate in table ',tabl[0], ' column ',tabl[1])
+                        # now look at the actual data and if its intersects
 
